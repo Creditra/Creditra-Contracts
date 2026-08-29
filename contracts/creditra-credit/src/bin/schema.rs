c402ea26f34a3d2497d69fcc5b15f3ba274771e1
OkpunTakem <okpuntakem50@gmail.com>
2026-08-29 21:10:45 +0100

    unit tests

--- contracts/creditra-credit/src/bin/schema.rs
+++ contracts/creditra-credit/src/bin/schema.rs
@@ -1,3 +1,8 @@
+// This binary is a host-side schema generator and must not be compiled for
+// the `wasm32` target when building the contract artifact. Guard it so CI
+// invocations that build the wasm target don't attempt to compile this file.
+#![cfg(not(target_arch = "wasm32"))]
+
 use cosmwasm_schema::write_api;
 #[allow(unused_imports)]
 use creditra_credit::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
