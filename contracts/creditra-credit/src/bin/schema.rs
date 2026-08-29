2d3061bac40ca33a0720e3d2ed9e1d0113cb2afc
OkpunTakem <okpuntakem50@gmail.com>
2026-08-29 20:55:23 +0100

    chore(schema): regenerate and commit schema JSON; remove unused import warning

--- contracts/creditra-credit/src/bin/schema.rs
+++ contracts/creditra-credit/src/bin/schema.rs
@@ -2,5 +2,9 @@ use cosmwasm_schema::write_api;
 use creditra_credit::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
-    write_api!();
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
 }
